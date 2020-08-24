445cb16720c2c0d46606f7a5c995d14c9fb4fb91
Ashley Sommer <ashleysommer@gmail.com>
2020-08-24 11:51:55 +1000

    allow pass in Linux version to build for

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+VERSION=$1
+if [ -z "$VERSION" ]; then
+    VERSION=`uname -r`
+fi
+
 cd "$(dirname "$(readlink -f "$0")")"
 
 # just to be sure the path is good
@@ -11,4 +16,4 @@ if quilt unapplied >/dev/null; then
 	quilt push -a
 fi
 
-exec make -C "/lib/modules/`uname -r`/build" M="$(pwd)"
+exec make -C "/lib/modules/$VERSION/build" M="$(pwd)"
